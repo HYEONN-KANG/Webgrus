2eb3cfd3c74681e73be8054d7f4ee296af16d239
kang hyeon <hyeonkang.dev@gmail.com>
2022-01-26 07:15:01 +0900

    공모전 이미지 소스 수정 및 서버 보안 기능 추가

--- server/server.js
+++ server/server.js
@@ -1,18 +1,31 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const api = require('./routes/api');
-const bodyParser = require('body-parser');
-const db = require('./config/db');
+const api = require("./routes/api");
+const bodyParser = require("body-parser");
+const db = require("./config/db");
 const port = 3001; // 포트 넘버
+
 app.use(express.json());
+
+// 보안 라이브러리 helmet
+const helmet = require("helmet");
+app.use(helmet());
+app.use(helmet.contentSecurityPolicy());
+app.use(helmet.hidePoweredBy());
+app.use(helmet.ieNoOpen());
+app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
+app.use(helmet.frameguard("deny"));
+
+app.disable("x-powered-by"); // expres가 사용되었다는 사실을 숨긴다
 
 // db 연결
 db.connect();
 
-app.use('/posters', express.static('client/public/posters'));
+app.use("/posters", express.static("client/public/posters"));
 app.use(bodyParser.json());
 
-app.use('/api', api);
+app.use("/api", api);
 
 // 서버 요청이랑 리액트 라우팅이랑 주소 경로가 겹치면 새로고침시 에러 나네요.
 // app.use('/webgrus', test);
